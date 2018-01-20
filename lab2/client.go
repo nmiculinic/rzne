d4f181cd343b13193a68393420b027591f41520e
Neven Miculinic <neven.miculinic@gmail.com>
2018-01-20 17:07:10 +0100

    Added code

--- lab2/client.go
+++ lab2/client.go
@@ -5,16 +5,7 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/nsqio/go-nsq"
 	"log"
-	"math/rand"
 	"net/http"
-	"sync"
-	"time"
-	"encoding/json"
-)
-
-var (
-	newline = []byte{'\n'}
-	space   = []byte{' '}
 )
 
 var upgrader = websocket.Upgrader{
@@ -22,33 +13,31 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-type Handler struct {
-	mutex sync.Mutex
-	conn  *websocket.Conn
+type Client struct {
+	conn     *websocket.Conn
+	addr     string
+	incoming chan []byte
 }
 
-func (h *Handler) HandleMessage(m *nsq.Message) error {
-	h.mutex.Lock()
-	defer h.mutex.Unlock()
-	return h.conn.WriteMessage(websocket.TextMessage, m.Body)
-}
-
-func wsHandler(cfg *nsq.Config) func(w http.ResponseWriter, r *http.Request) {
+func wsHandler(producer *nsq.Producer, hub *Hub) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		conn, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
 			log.Println(err)
 			return
 		}
+		defer conn.Close()
 
-		v, _ := json.MarshalIndent(cfg, "", "\t")
-		log.Println(string(v))
-		producer, err:= nsq.NewProducer("127.0.0.1:4150", cfg)
-		if err != nil {
-			log.Println(err)
-			return
+		client := Client{
+			addr:     fmt.Sprint(conn.RemoteAddr()),
+			conn:     conn,
+			incoming: make(chan []byte, 256),
 		}
+		hub.register <- &client
+		defer func(){hub.unregister <- &client}()
+
 		go func() {
+			defer func(){hub.unregister <- &client}()
 			for {
 				_, message, err := conn.ReadMessage()
 				if err != nil {
@@ -57,19 +46,17 @@ func wsHandler(cfg *nsq.Config) func(w http.ResponseWriter, r *http.Request) {
 					}
 					break
 				}
-				producer.Publish("test", message)
+				if err := producer.Publish("test", message); err != nil {
+					log.Println(err)
+					break
+				}
 			}
 		}()
-		rand.Seed(time.Now().UnixNano())
-		channel := fmt.Sprintf("client%06d#ephemeral", rand.Int()%999999)
 
-		if consumer, err := nsq.NewConsumer("test", channel, cfg); err != nil {
-			log.Println(err)
-		} else {
-			consumer.AddHandler(&Handler{conn: conn})
-			err = consumer.ConnectToNSQLookupd(*nsqdHTTPAddrs)
-			if err != nil {
-				log.Fatal(err)
+		for msg := range client.incoming {
+			if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+				log.Println(err)
+				break
 			}
 		}
 	}
--- lab2/main.go
+++ lab2/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/nsqio/go-nsq"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 var (
-	addr          = flag.String("addr", ":8080", "http service address")
-	nsqdHTTPAddrs = flag.String("nsqd-http-address", "", "nsqd-http-adress")
+	addr                   = flag.String("addr", ":8080", "http service address")
+	nsqdLookupdHTTPAddress = flag.String("nsqd-lookupd-http-address", "127.0.0.1:4161", "nsqd-lookupd-tcp-address")
+	nsqdTCPAddress         = flag.String("nsqd-tcp-address", "127.0.0.1:4150", "nsqd-tcp-address")
 )
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
@@ -25,17 +29,57 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "home.html")
 }
 
+type nsqHandler struct {
+	hub *Hub
+}
+
+func (h *nsqHandler) HandleMessage(m *nsq.Message) error {
+	log.Println("Got", string(m.Body))
+	select {
+	case h.hub.broadcast <- m.Body:
+		return nil
+	default:
+		return fmt.Errorf("cannot push message to hub")
+	}
+}
+
 func main() {
 	cfg := nsq.NewConfig()
 	flag.Var(&nsq.ConfigFlag{cfg}, "consumer-opt", "option to passthrough to nsq.Consumer (may be given multiple times, http://godoc.org/github.com/nsqio/go-nsq#Config)")
 	flag.Parse()
 
-	if len(*nsqdHTTPAddrs) == 0 {
-		log.Fatal("--nsqd-http-address required")
+	producer, err := nsq.NewProducer(*nsqdTCPAddress, cfg)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for {
+		if err := producer.Ping(); err != nil {
+			log.Println(err)
+			time.Sleep(time.Second)
+			continue
+		}
+		break
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	channel := fmt.Sprintf("client%06d#ephemeral", rand.Int()%999999)
+	hub := newHub()
+	go hub.run()
+
+	if consumer, err := nsq.NewConsumer("test", channel, cfg); err != nil {
+		log.Println(err)
+	} else {
+		consumer.AddHandler(&nsqHandler{hub:hub})
+		err = consumer.ConnectToNSQLookupd(*nsqdLookupdHTTPAddress)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", wsHandler(cfg))
+	http.HandleFunc("/ws", wsHandler(producer, hub))
 	log.Printf("Serving on http://localhost%s/\n", *addr)
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
